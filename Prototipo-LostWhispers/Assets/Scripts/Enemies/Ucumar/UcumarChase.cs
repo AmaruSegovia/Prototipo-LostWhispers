211a0a69ef8fefc7bb012d748fd9c4593ca5475f
Cristian Arraya <arrayacristian75@gmail.com>
2024-06-18 10:36:03 -0300

    Agrego movimiento a la camara a caminar, nuevo sonidos de pasos, salto, aterrizaje y se modifico el script PlayerController

--- Prototipo-LostWhispers/Assets/Scripts/Enemies/Ucumar/UcumarChase.cs
+++ Prototipo-LostWhispers/Assets/Scripts/Enemies/Ucumar/UcumarChase.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.UIElements;
+using UnityStandardAssets.Characters.FirstPerson;
 
 public class EnemyAI : MonoBehaviour
 {
