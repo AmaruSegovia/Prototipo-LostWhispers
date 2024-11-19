65fa120be5256271bea6a0583d27e2a03d8c448d
Ismael Ignacio Miguel Torres <ignacio25.it@gmail.com>
2024-11-19 16:47:53 -0300

    Wwise implementado

---
+++ Prototipo-LostWhispers/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,44 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID ACTIVARVIOLINES = 2508554500U;
+        static const AkUniqueID AMBIENTE = 4095160060U;
+        static const AkUniqueID CHARCOINOUT = 3823442842U;
+        static const AkUniqueID MONSTRUO = 3542090678U;
+        static const AkUniqueID OVEJA = 2247432076U;
+        static const AkUniqueID PUERTA = 3831537568U;
+        static const AkUniqueID RADIO = 2548238350U;
+    } // namespace EVENTS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MAINBANK = 2880737896U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
